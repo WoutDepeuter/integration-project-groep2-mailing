9794728e236c453efad2dfcfc61e605b70de2152
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-03-30 17:30:50 +0200

    Added rabbitmq email thing
    emails can normally be send using xml in rabbitmq if we publish a xml message in the queue "mailing.mail" for example:
    
    it fails if it doesnt receive sender in xml eventhough we overwrite it, idk why might be a sendgrid requirement
    
    <GenericEmail>
        <header>
            <sender>
                <name>Sender name</name>
                <email>sender.email@example.com</email>
            </sender>
            <recipients>
                <recipient>
                    <name>Tristan Vong</name>
                    <email>tristanvong1@gmail.com</email>
                </recipient>
            </recipients>
            <cc>
                <recipient>
                    <email>cc@example.com</email>
                </recipient>
            </cc>
            <bcc>
                <recipient>
                    <email>bcc@example.com</email>
                </recipient>
            </bcc>
            <subject>Test Email</subject>
        </header>
        <body>
            <content>Your mail content goes here</content>
            <contentType>TEXT_PLAIN</contentType>
        </body>
    </GenericEmail>

--- src/main/java/ehb/attendify/services/mailingservice/controllers/GenericMailingController.java
+++ src/main/java/ehb/attendify/services/mailingservice/controllers/GenericMailingController.java
@@ -1,6 +1,7 @@
 package ehb.attendify.services.mailingservice.controllers;
 
 import ehb.attendify.services.mailingservice.models.GenericEmail;
+import ehb.attendify.services.mailingservice.services.api.EmailService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class GenericMailingController {
 
+    private final EmailService emailService;
+
+    public GenericMailingController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "mailing.mail")
     public void onGenericMail(GenericEmail email) {
         log.info("We've got an email! From {}, to {}", email.getHeader().getSender().getName(), email.getHeader().getRecipients());
+        emailService.sendEmail(email);
     }
 
 }
