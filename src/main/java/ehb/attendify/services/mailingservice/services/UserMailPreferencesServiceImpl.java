869cb1dcb25672ecc99846bb72675b827546568d
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-03-30 18:52:42 +0200

    Removed demo in UserMailPreferencesServiceImpl.java

--- src/main/java/ehb/attendify/services/mailingservice/services/UserMailPreferencesServiceImpl.java
+++ src/main/java/ehb/attendify/services/mailingservice/services/UserMailPreferencesServiceImpl.java
@@ -1,11 +1,7 @@
 package ehb.attendify.services.mailingservice.services;
 
 import ehb.attendify.services.mailingservice.dto.UserMailPreferencesDto;
-import ehb.attendify.services.mailingservice.models.GenericEmail;
 import ehb.attendify.services.mailingservice.models.UserMailPreferences;
-import ehb.attendify.services.mailingservice.models.mail.body.Body;
-import ehb.attendify.services.mailingservice.models.mail.header.Header;
-import ehb.attendify.services.mailingservice.models.mail.header.Recipient;
 import ehb.attendify.services.mailingservice.repositories.UserMailPreferencesRepository;
 import ehb.attendify.services.mailingservice.services.api.EmailService;
 import ehb.attendify.services.mailingservice.services.api.UserMailPreferencesService;
@@ -51,30 +47,6 @@ public class UserMailPreferencesServiceImpl implements UserMailPreferencesServic
         }
 
         updatedPref = this.userMailPreferencesRepository.save(updatedPref);
-
-        Header header = new Header();
-        // example of two recipients with one cc
-        Recipient recipient = new Recipient();
-        recipient.setEmail("tristanvong1@gmail.com");
-
-        Recipient recipient2 = new Recipient();
-        recipient2.setEmail("tristanvong.ehb@gmail.com");
-
-        Recipient ccRecipient = new Recipient();
-        ccRecipient.setEmail("test@gmail.com");
-
-        header.setRecipients(List.of(recipient, recipient2));
-        header.setCc(List.of(ccRecipient));
-        header.setSubject("Mail Preferences Updated");
-
-        Body body = new Body();
-        body.setContent("Your mail preferences have been updated to: " + updatedPref.getMailGreetingType());
-
-        GenericEmail email = new GenericEmail();
-        email.setHeader(header);
-        email.setBody(body);
-
-        emailService.sendEmail(email);
 
         return updatedPref;
     }
