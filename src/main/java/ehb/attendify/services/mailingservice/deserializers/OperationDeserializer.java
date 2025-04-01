1d9ed56733f4fe7305dfa0d3f3c02426889ee2e0
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-04-01 12:06:56 +0200

    Ignore case when getting operation

---
+++ src/main/java/ehb/attendify/services/mailingservice/deserializers/OperationDeserializer.java
@@ -0,0 +1,27 @@
+package ehb.attendify.services.mailingservice.deserializers;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import ehb.attendify.services.mailingservice.models.enums.Operation;
+
+import java.io.IOException;
+
+public class OperationDeserializer extends JsonDeserializer<Operation> {
+    @Override
+    public Operation deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        String value = p.getText();
+        if (value == null) {
+            return null;
+        }
+
+        for (Operation operation : Operation.values()) {
+            if (value.equalsIgnoreCase(operation.name())) {
+                return operation;
+            }
+        }
+
+        return null;
+    }
+}
--- src/main/java/ehb/attendify/services/mailingservice/models/enums/Operation.java
+++ src/main/java/ehb/attendify/services/mailingservice/models/enums/Operation.java
@@ -1,5 +1,9 @@
 package ehb.attendify.services.mailingservice.models.enums;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import ehb.attendify.services.mailingservice.deserializers.OperationDeserializer;
+
+@JsonDeserialize(using = OperationDeserializer.class)
 public enum Operation {
     CREATE,
     UPDATE,
