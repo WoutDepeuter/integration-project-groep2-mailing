d3966c1dc7aa71517fab2aa1ed7a0a66d06ef344
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-04-03 22:57:29 +0200

    Fix cherry pick
    dit was niet included somehow oops

--- src/main/java/ehb/attendify/services/mailingservice/models/general/AttendifyMessage.java
+++ src/main/java/ehb/attendify/services/mailingservice/models/general/AttendifyMessage.java
@@ -1,7 +1,6 @@
 package ehb.attendify.services.mailingservice.models.general;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import ehb.attendify.services.mailingservice.models.user.User;
 import lombok.Data;
 
 @Data
@@ -10,6 +9,6 @@ public class AttendifyMessage<T> {
 
     private Info info;
 
-    private User user;
+    private T payload;
 
 }
